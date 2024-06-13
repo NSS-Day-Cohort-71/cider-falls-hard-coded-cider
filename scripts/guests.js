68c74afe99ad3580426e578dfa20922182fada74
Jana Ismail <166757460+Jana-Ismail@users.noreply.github.com>
2024-06-13 14:19:29 -0500

    Update guests.js

--- scripts/guests.js
+++ scripts/guests.js
@@ -3,13 +3,13 @@ import { getGuests } from './database.js'
 
 const guests = getGuests()
 
-export const GuestsList = () => {
+export const Guests = () => {
     let guestsHTML = `<ul>`
 
     for (const guest of guests) {
         guestsHTML += `<li
                 data-type="guest"
-                data-id="${guest.firstName}"
+                data-id="${guest.id}"
                 data-area-id="${guest.parkAreaId}"
                 data-first-name="${guest.firstName}"
                 data-last-name="${guest.lastName}"
