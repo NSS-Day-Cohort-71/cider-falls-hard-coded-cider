f45e2138e25fe38b3eaf93f8251bbd9b670ccd57
Zachary Thacker <zthacker@outlook.com>
2024-06-13 13:36:52 -0500

    started area code

--- scripts/areas.js
+++ scripts/areas.js
@@ -1,13 +1,23 @@
 // import the getFunctions from Database
-
+import { getAreas } from "./database.js";
 // reminder for later: click events -- not now
 
 // invoke getFunction
-
+const areas = getAreas();
 // create an export function for creating AreaHTML
+export const AreaHTML = () => {
+  let AreaHTML = "<ol>";
+  // invoke iterate through the array of objects
+  for (const area of areas) {
+    // create html portion
+    AreaHTML += `
+    <li>
+    ${area.name}
+    </li>
+    `;
+  }
 
-// invoke iterate through the array of objects
-
-// create html portion
-
-// return html
+  AreaHTML += "</ol>";
+  // return html
+  return AreaHTML;
+};
