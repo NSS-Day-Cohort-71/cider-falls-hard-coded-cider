20e23bfa2ea335bf6bf8e979635742f939bd7a33
Zachary Thacker <zthacker@outlook.com>
2024-06-13 15:49:16 -0500

    started findAreaServies function

--- scripts/areas.js
+++ scripts/areas.js
@@ -1,9 +1,10 @@
 // import the getFunctions from Database
-import { getAreas, getParkAreaServices } from "./database.js";
+import { getAreas, getParkAreaServices, getServices } from "./database.js";
 // reminder for later: click events -- not now
 
 // invoke getFunction
 const areas = getAreas();
+const allServices = getServices();
 const parkAreaServices = getParkAreaServices();
 // create an export function for creating AreaHTML
 export const AreaHTML = () => {
@@ -23,14 +24,24 @@ export const AreaHTML = () => {
   return AreaHTML;
 };
 
+//fix this tomorrow!!
+
 const findAreaServices = (area, parkAreaServices) => {
+  let serviceIds = [];
   let currentServices = {};
 
   for (const service of parkAreaServices) {
     if (service.parkAreaId === area.parkAreaId) {
-      currentServices = service;
+      serviceIds.push(service.serviceId);
+    }
+    for (const serviceId of serviceIds) {
+      if (serviceId === allServices.id) {
+        currentServices.push(allServices.name);
+      }
     }
   }
 
   return currentServices;
+
+  //add to AreaHTML
 };
