0093f0fa92844e447f273def5114d764fe86bab7
Jana Ismail <janaismail@janas-air.lan>
2024-06-13 14:03:29 -0500

    Created GuestsList() component function

--- scripts/guests.js
+++ scripts/guests.js
@@ -1,1 +1,22 @@
 // import the getFunctions from Database
+import { getGuests } from './database.js'
+
+const guests = getGuests()
+
+export const GuestsList = () => {
+    let guestsHTML = `<ul>`
+
+    for (const guest of guests) {
+        guestsHTML += `<li
+                data-type="guest"
+                data-id="${guest.firstName}"
+                data-area-id="${guest.parkAreaId}"
+                data-first-name="${guest.firstName}"
+                data-last-name="${guest.lastName}"
+            >${guest.firstName} ${guest.lastName}</li>`
+    }
+
+    guestsHTML += `</ul>`
+
+    return guestsHTML
+}
