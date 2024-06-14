d0c35c132493095d87846b5bb88058d45e54fa78
Michael Galo <michael@michaelthinks.co>
2024-06-14 11:50:51 -0500

    services banner click event added

--- scripts/services.js
+++ scripts/services.js
@@ -1,10 +1,35 @@
 // import the getServices from Database
-import { getServices } from "./database.js";
+import { getAreas, getParkAreaServices, getServices } from "./database.js";
+import { findAreaServices } from "./areas.js";
 
-// reminder for later: click events -- not now
-
-// invoke getFunction and set it to a variable
+const areas = getAreas();
+const parkAreaServices = getParkAreaServices();
 const services = getServices();
+
+// create click event listener for the bannerServicesContainer
+document.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.dataset.type === "banner-service") {
+    const clickedName = clickEvent.target.dataset.name;
+    let currentAreasWithClickedService = [];
+    let areaServicesHTMLString = "";
+
+    for (const area of areas) {
+      const areaServices = findAreaServices(
+        area,
+        getParkAreaServices(),
+        services
+      );
+
+      if (areaServices.includes(clickedName)) {
+        currentAreasWithClickedService.push(area.name);
+      }
+    }
+
+    areaServicesHTMLString += currentAreasWithClickedService.join(", ");
+
+    window.alert(`${clickedName} is available in ${areaServicesHTMLString}`);
+  }
+});
 
 // create function that returns an HTML string of the services
 export const bannerServices = () => {
@@ -13,7 +38,7 @@ export const bannerServices = () => {
 
   for (const service of services) {
     serviceArray.push(
-      `<span data-type="service" data-id=${service.id} data-name=${service.name}> ${service.name}</span>`
+      `<span data-type="banner-service" data-id=${service.id} data-name=${service.name}> ${service.name}</span>`
     );
   }
 
@@ -27,7 +52,6 @@ export const bannerServices = () => {
 export const Services = () => {
   let html = `<article class="services">`;
 
-  // iterate through the array of services
   for (const service of services) {
     html += `
     <ul>
