09c90a12e24f22d409f2c18e2cff314e8c664416
Zachary Thacker <zthacker@outlook.com>
2024-06-14 10:36:13 -0500

    fixed findAreaServices

--- scripts/areas.js
+++ scripts/areas.js
@@ -2,46 +2,50 @@
 import { getAreas, getParkAreaServices, getServices } from "./database.js";
 // reminder for later: click events -- not now
 
-// invoke getFunction
+// invoke getFunctions
 const areas = getAreas();
 const allServices = getServices();
 const parkAreaServices = getParkAreaServices();
+
+// create a function to find services for a specific area
+const findAreaServices = (area, parkAreaServices, allServices) => {
+  let serviceIds = [];
+  let currentServices = [];
+
+  for (const service of parkAreaServices) {
+    if (service.parkAreaId === area.id) {
+      serviceIds.push(service.serviceId);
+    }
+  }
+
+  for (const serviceId of serviceIds) {
+    for (const service of allServices) {
+      if (service.id === serviceId) {
+        currentServices.push(service.name);
+      }
+    }
+  }
+
+  return currentServices;
+};
+
 // create an export function for creating AreaHTML
 export const AreaHTML = () => {
   let AreaHTML = "<article>";
-  // invoke iterate through the array of objects
+
+  // iterate through the array of areas
   for (const area of areas) {
-    // create html portion
+    const services = findAreaServices(area, parkAreaServices, allServices);
+
     AreaHTML += `
-    <h2>${area.name}</h2>
-    <div class="services-offered">
-    </div>
+      <h2>${area.name}</h2>
+      <div class="services-offered">
+        ${services.map((service) => `<p>${service}</p>`).join("")}
+      </div>
     `;
   }
 
   AreaHTML += "</article>";
   // return html
   return AreaHTML;
-};
-
-//fix this tomorrow!!
-
-const findAreaServices = (area, parkAreaServices) => {
-  let serviceIds = [];
-  let currentServices = {};
-
-  for (const service of parkAreaServices) {
-    if (service.parkAreaId === area.parkAreaId) {
-      serviceIds.push(service.serviceId);
-    }
-    for (const serviceId of serviceIds) {
-      if (serviceId === allServices.id) {
-        currentServices.push(allServices.name);
-      }
-    }
-  }
-
-  return currentServices;
-
-  //add to AreaHTML
 };
